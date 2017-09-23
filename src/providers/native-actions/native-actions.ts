2934805305d1acd93d1a8da339198c4e65606170
brenopolanski <breno.polanski@gmail.com>
2017-09-23 14:27:24 -0300

    Update native-actions.ts

--- src/providers/native-actions/native-actions.ts
+++ src/providers/native-actions/native-actions.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Globalization } from '@ionic-native/globalization';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 import { Vibration } from '@ionic-native/vibration';
 
 @Injectable()
 export class NativeActionsProvider {
 
-  // preferredLanguage: string = 'en';
-
   constructor(
-    private globalization: Globalization,
     private tts: TextToSpeech,
     private vibration: Vibration
   ) {
-/*     this.globalization.getPreferredLanguage()
-      .then(res => this.preferredLanguage = res.value)
-      .catch(error => console.log(error)); */
+
   }
 
   playAudio(text, locale = 'pt-BR') {
